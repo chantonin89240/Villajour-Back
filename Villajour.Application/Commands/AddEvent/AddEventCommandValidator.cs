cb5dff4b3dcddd4a3f3f88bb5d0d259897a38169
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2024-05-27 16:07:45 +0200

    Ajout du controller pour les events

---
+++ Villajour.Application/Commands/AddEvent/AddEventCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Villajour.Application.Commands.AddMairie;
+
+public class AddEventCommandValidator : AbstractValidator<AddEventCommand>
+{
+    public AddEventCommandValidator()
+    {
+        RuleFor(c => c.StartTime).NotEmpty();
+        RuleFor(c => c.Title).NotEmpty();
+        RuleFor(c => c.Address).NotEmpty();
+        RuleFor(c => c.EventTypeId).NotEmpty();
+        RuleFor(c => c.MairieId).NotEmpty();
+    }
+}
