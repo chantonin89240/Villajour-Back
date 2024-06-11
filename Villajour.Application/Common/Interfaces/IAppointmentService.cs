77ba2dd400d9cd54e5d22564810698c05bf8b986
Kriqx <jeanchristophe.pouzin@diiage.org>
2024-06-11 15:15:47 +0200

    Appointment

---
+++ Villajour.Application/Common/Interfaces/IAppointmentService.cs
@@ -0,0 +1,11 @@
+﻿using Villajour.Domain.Common;
+using System.Collections.Generic;
+
+public interface IAppointmentService
+{
+    Task<IEnumerable<AppointmentEntity>> GetAllAppointmentsAsync();
+    Task<AppointmentEntity> GetAppointmentByIdAsync(int id);
+    Task<AppointmentEntity> AddAppointmentAsync(AppointmentEntity appointment);
+    Task<AppointmentEntity> UpdateAppointmentAsync(AppointmentEntity appointment);
+    Task DeleteAppointmentAsync(int id);
+}
---
+++ Villajour.Persistence/Repositories/AppointmentRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Villajour.Domain.Common;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Villajour.Persistence.Interfaces;
+using Villajour.Application.Common.Interfaces;
+
+namespace Villajour.Persistence.Repositories
+{
+    public class AppointmentRepository : Application.Common.Interfaces.IAppointmentRepository
+    {
+        private readonly VilleajourDbContext _context;
+
+        public AppointmentRepository(VilleajourDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<AppointmentEntity>> GetAllAppointmentsAsync()
+        {
+            return await _context.Appointments.ToListAsync();
+        }
+
+        public async Task<AppointmentEntity> GetAppointmentByIdAsync(int id)
+        {
+            return await _context.Appointments.FindAsync(id);
+        }
+
+        public async Task<AppointmentEntity> AddAppointmentAsync(AppointmentEntity appointment)
+        {
+            _context.Appointments.Add(appointment);
+            await _context.SaveChangesAsync();
+            return appointment;
+        }
+
+        public async Task<AppointmentEntity> UpdateAppointmentAsync(AppointmentEntity appointment)
+        {
+            _context.Entry(appointment).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return appointment;
+        }
+
+        public async Task DeleteAppointmentAsync(int id)
+        {
+            var appointment = await _context.Appointments.FindAsync(id);
+            if (appointment != null)
+            {
+                _context.Appointments.Remove(appointment);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
