6d194fd5953d4c1396e02806ea53ab99ae5c456a
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2024-06-20 14:07:25 +0200

    fonction de récupération des mairies fav d'un utilisateur

---
+++ Villajour.Application/Commands/Users/GetMairieFavByUser/GetMairieFavByUserCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Villajour.Application.Commands.Events.GetEventByMairieDetail;
+
+namespace Villajour.Application.Commands.Users.GetMairieFavByUser;
+
+public class GetMairieFavByUserCommandValidator : AbstractValidator<GetMairieFavByUserCommand>
+{
+    public GetMairieFavByUserCommandValidator()
+    {
+        RuleFor(c => c.UserId).NotEmpty();
+    }
+}
